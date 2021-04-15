89e41e85926a2dc53e294ebd71eb451de58724dd
amsvvi <80904338+amsvvi@users.noreply.github.com>
2021-04-15 17:48:47 +0300

    Add files via upload

--- js/myjs.js
+++ js/myjs.js
@@ -4,7 +4,7 @@ $(function () {
 
     if ($(window).height() > 900) {
         $(".coffe_animation_block").addClass("big_m");
-        $(".txt-cont").css("min-height", $(window).height() - 140 +"px");
+    //    $(".txt-cont").css("min-height", $(window).height() - 140 +"px");
     }
     else {
         $(".txt-cont-6").addClass("block-h6");
